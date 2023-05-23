ccc61818cfa5ed53799a9b68b357504f2f8780ac
popeskul <peterjj91@gmail.com>
2023-05-23 16:06:59 +0300

    refactor(main.go): remove initConfig function and move its functionality to config.New() and loadEnv() functions feat(main.go): add support for loading environment variables using viper package to allow for more flexible configuration options

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/joho/godotenv"
 	"github.com/nkolosov/whip-round/internal/app"
 	"github.com/nkolosov/whip-round/internal/config"
 	"github.com/nkolosov/whip-round/internal/health"
-	"log"
-	"os"
-	"os/signal"
-	"strconv"
-	"syscall"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	cfg, err := initConfig()
+	err := loadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := config.New()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,52 +47,13 @@ func main() {
 	log.Fatal(srv.Shutdown(context.Background()))
 }
 
-func initConfig() (*config.Config, error) {
-	if err := godotenv.Load(".env"); err != nil {
-		return nil, fmt.Errorf("error while loading .env file: %w", err)
-	}
-
-	cfg, err := config.New()
+func loadEnv() error {
+	err := godotenv.Load(".env")
 	if err != nil {
-		return nil, fmt.Errorf("error while reading config: %w", err)
+		return fmt.Errorf("error while loading .env file: %w", err)
 	}
 
-	port, err := strconv.Atoi(os.Getenv("APP_PORT"))
-	if err != nil {
-		return nil, fmt.Errorf("error while reading port from env: %w", err)
-	}
-	cfg.Server.Port = port
+	viper.AutomaticEnv()
 
-	healthCheckPort, err := strconv.Atoi(os.Getenv("HEALTH_CHECK_PORT"))
-	if err != nil {
-		return nil, fmt.Errorf("error while reading health check port from env: %w", err)
-	}
-	cfg.Server.HealthCheckPort = healthCheckPort
-
-	cfg.TokenSymmetricKey = os.Getenv("TOKEN_SYMMETRIC_KEY")
-	cfg.HashSalt = os.Getenv("HASH_SALT")
-
-	cfg.DB.Host = os.Getenv("DB_HOST")
-	dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
-	if err != nil {
-		return nil, err
-	}
-	cfg.DB.Port = dbPort
-	cfg.DB.User = os.Getenv("DB_USER")
-	cfg.DB.DBName = os.Getenv("DB_NAME")
-	cfg.DB.Password = os.Getenv("DB_PASSWORD")
-	cfg.DB.SSLMode = os.Getenv("DB_SSL_MODE")
-
-	cfg.DB.MaxIdleConns, err = strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNS"))
-	if err != nil {
-		return nil, err
-	}
-	cfg.DB.MaxOpenConns, err = strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONNS"))
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Printf("%+v\n", cfg)
-
-	return cfg, nil
+	return nil
 }
