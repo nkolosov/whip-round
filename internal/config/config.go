256bf9b71bfa1d18bf5a4fd9be81d407708b7864
popeskul <peterjj91@gmail.com>
2023-05-23 16:13:49 +0300

    db-migration-models: updated Config with viper

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,6 @@
 package config
 
-import (
-	"github.com/kelseyhightower/envconfig"
-)
+import "github.com/spf13/viper"
 
 type Config struct {
 	Server struct {
@@ -28,21 +26,18 @@ type PostgresConfig struct {
 func New() (*Config, error) {
 	cfg := &Config{}
 
-	if err := envconfig.Process("server", cfg); err != nil {
-		return nil, err
-	}
-
-	if err := envconfig.Process("token", cfg); err != nil {
-		return nil, err
-	}
-
-	if err := envconfig.Process("hash", cfg); err != nil {
-		return nil, err
-	}
-
-	if err := envconfig.Process("db", &cfg.DB); err != nil {
-		return nil, err
-	}
+	cfg.Server.Port = viper.GetInt("APP_PORT")
+	cfg.Server.HealthCheckPort = viper.GetInt("HEALTH_CHECK_PORT")
+	cfg.TokenSymmetricKey = viper.GetString("TOKEN_SYMMETRIC_KEY")
+	cfg.HashSalt = viper.GetString("HASH_SALT")
+	cfg.DB.Host = viper.GetString("DB_HOST")
+	cfg.DB.Port = viper.GetInt("DB_PORT")
+	cfg.DB.User = viper.GetString("DB_USER")
+	cfg.DB.Password = viper.GetString("DB_PASSWORD")
+	cfg.DB.DBName = viper.GetString("DB_NAME")
+	cfg.DB.SSLMode = viper.GetString("DB_SSL_MODE")
+	cfg.DB.MaxOpenConns = viper.GetInt("DB_MAX_OPEN_CONNS")
+	cfg.DB.MaxIdleConns = viper.GetInt("DB_MAX_IDLE_CONNS")
 
 	return cfg, nil
 }
