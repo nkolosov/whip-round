0cfc453e9bb9f6e145497df2895ae5764972a43e
popeskul <peterjj91@gmail.com>
2023-05-23 16:35:33 +0300

    db-migration-models: added undexes to sql

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,16 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Создаем таблицу users
 CREATE TABLE users
 (
     id         UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    login      VARCHAR(255),
-    email      VARCHAR(255),
+    login      VARCHAR(255) UNIQUE,
+    email      VARCHAR(255) UNIQUE,
     birthdate  DATE,
-    phone      VARCHAR(20),
+    phone      VARCHAR(20) UNIQUE,
     balance    BIGINT,
     created_at TIMESTAMP
 );
+
+CREATE INDEX idx_users_login ON users (login);
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_phone ON users (phone);
 
 CREATE TABLE refresh_sessions
 (
@@ -19,3 +22,5 @@ CREATE TABLE refresh_sessions
     token      VARCHAR(255) NOT NULL,
     expires_at TIMESTAMP    NOT NULL
 );
+
+CREATE INDEX idx_refresh_sessions_user_id ON refresh_sessions (user_id);
